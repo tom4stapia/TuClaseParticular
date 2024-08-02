223afc9cff21776524d78ddb7099644b74a84e65
TOMAS TAPIA <tomastapia@uc.cl>
2024-08-02 15:03:47 -0400

    metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,19 @@ const dmSans = DM_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Tu Clase Particular",
-  description: "Tu Clase Particular",
+  description: "¿Estás buscando una clase particular para tu hijo? Agenda tu clase con nosotros!",
+  openGraph: {
+    title: "Tu Clase Particular",
+    description: "¿Estás buscando una clase particular para tu hijo? Agenda tu clase con nosotros!",
+    url: "https://www.tuclaseparticular.cl",
+    type: "website",
+    images: [
+      {
+        url: "https://i.postimg.cc/P5zvwKDr/prev-1.webp",
+        alt: "Tu Clase Particular",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
@@ -17,6 +29,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="relative">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="google-site-verification" content="7P_f8EA2asYmee8a4EC6KZEfY21ffBRCi1WkZ3q0Hks" />
+      </head>
       <body className={clsx(dmSans.className, "antialiased bg-[#EAEEFE]")}>
         {children}
       </body>
